1d090af9af1db288ae5d5af57150458254a48911
pinnnguyen <pinnguyen0597@gmail.com>
2023-03-20 11:18:20 +0700

    Updated meta tags to provide better control of viewport for mobile devices.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,14 @@ export default defineNuxtConfig({
     app: {
         head: {
             charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
             htmlAttrs: {
                 lang: 'en',
             },
             meta: [
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+                },
                 { hid: 'title', name: 'title', content: 'MenuChildrenGPT' },
                 { hid: 'description', name: 'description', content: 'Create a menu for baby in seconds with chatGPT.' },
                 { hid: 'og:title', property: 'og:title', content: 'MenuChildrenGPT.' },
